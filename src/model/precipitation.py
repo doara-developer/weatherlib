f9e8ed63b870361a3f4ce079328e2d619054f71d
doaradev <doaradev@gmail.com>
2019-09-25 00:25:04 +0900

    Add weatherlib

---
+++ src/model/precipitation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+class Precipitation():
+    """Precipitation Model
+    Precipitation model.
+
+    Attributes:
+    data (list): response from yahoo api
+    """
+    data = []
+
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+    @property
+    def current(self):
+        return self.data[0]['Date']
+
+    @property
+    def is_rain(self):
+        return any(elem['Rainfall'] > 0 for elem in self.data)
+
+    @property
+    def detail(self):
+        cur_date = self._convert_datetime(self.data[0]['Date'])
+        ret = [
+            {
+                'time': int((self._convert_datetime(d['Date']) - cur_date).seconds/60),
+                'rain_fall': d['Rainfall']
+            }
+            for d in self.data
+        ]
+        return ret
+
+    def _convert_datetime(self, datestr):
+        return datetime.datetime.strptime(datestr, '%Y%m%d%H%M')
---
+++ src/weatherlib.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+from .model.precipitation import Precipitation
+
+LOCATION_TEMPLATE = {
+    'sapporo': [141.350755, 43.068661],
+    'sendai': [140.882438, 38.260132],
+    'tokyo': [139.767125, 35.681236],
+    'mishima': [138.911126, 35.126299],
+    'nagoya': [136.881537, 35.170915],
+    'kanazawa': [136.648171, 36.578044],
+    'osaka': [135.495951, 34.702485],
+    'hakata': [130.420727, 33.589728],
+    'naha': [127.652184, 26.206481]
+}
+
+
+class WeatherLib():
+    """WeatherLib
+    Get weather information by using yahoo api
+
+    Attributes:
+    id (str): application id for yahoo api
+    """
+    id = ''
+    url = "https://map.yahooapis.jp/weather/V1/place"
+
+    def __init__(self, id):
+        self.id = id
+
+    def get_precipitation(self, location=None, latitude=0.0, longitude=0.0, interval=5):
+        """
+        Get precipitation for specified location.
+
+        Args:
+        location (str): location name.
+        latitude (float): location latitude. If set 'location', this parameter is ignored.
+        longitude (float): location longitude. If set 'location', this parameter is ignored.
+
+        Returns:
+        Precipitation: precipitation info
+        """
+        if location is not None and location in LOCATION_TEMPLATE:
+            coordinates = str(LOCATION_TEMPLATE[location][0]) + ',' + str(LOCATION_TEMPLATE[location][1])
+        else:
+            coordinates = str(latitude) + ',' + str(longitude)
+
+        payload = {
+            'coordinates': coordinates,
+            'appid': self.id,
+            'output': 'json',
+            'interval': str(interval)
+        }
+        r = requests.get(self.url, params=payload)
+        data = json.loads(r.text)
+
+        return Precipitation(data['Feature'][0]['Property']['WeatherList']['Weather'])
